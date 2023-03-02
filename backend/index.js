09f0ba1a01a7b7affe21f05adc5e5f2c95a1d64b
Valerio Franchi <vlrfranchi1@gmail.com>
2023-03-02 12:46:20 +0000

    Mongodb issue solved"

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import app from "./server.js"
-import mondodb from "./mongodb"
+import mongodb from "mongodb"
 import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -9,13 +9,13 @@ const port = process.env.PORT || 8000
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
-        poolSize: 50,
-        wtimeout: 2500,
-        useNewUrlParse: true }
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true }
 )
 
 .catch(err => {
-    console.error (err.stack)
+    console.error(err.stack)
     process.exit
 })
 .then(async client => {
